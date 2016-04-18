b5d011cd0642be4b3fb51982b1a1c82b769c2a46
viktorstrate <viktorstrate@gmail.com>
2016-04-18 18:43:00 +0200

    Fixed gradle files, added server module

---
+++ server/src/dk/qpqp/server/ServerLauncher.java
@@ -0,0 +1,10 @@
+package dk.qpqp.server;
+
+/**
+ * Created by viktorstrate on 18/04/16.
+ */
+public class ServerLauncher {
+    public static void main(String[] args) {
+        System.out.println("Start server here");
+    }
+}
